b42df7048ed4fcbfec3ed845af5ae4637bb99794
Tigran Tovmasyan <tikotovmasyan@Air-Tigran.lan>
2025-04-19 16:50:46 +0300

    prepare for deploy

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+fxichi(e#clvl4jwmkb=w8s+m84^pb(dotqhlh_-mz$(e5!0f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+import os
 
-ALLOWED_HOSTS = []
+DEBUG = os.environ.get("DEBUG", "False") == "True"
+ALLOWED_HOSTS = ['your-app-name.onrender.com']
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Application definition
 
@@ -48,6 +50,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
