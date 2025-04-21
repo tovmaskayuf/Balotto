b3cabcd7b395d268e2c0940f41ec4685a4ec657c
Tigran Tovmasyan <tikotovmasyan@Air-Tigran.lan>
2025-04-21 11:18:57 +0300

    new

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,7 +30,11 @@ import os
 DEBUG = os.environ.get("DEBUG", "False") == "True"
 ALLOWED_HOSTS = ['balotto.onrender.com']
 
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Application definition
 
--- mysite/urls.py
+++ mysite/urls.py
@@ -29,4 +29,5 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
